c967aab6405226c7fc01fb583374c3506f77e5d8
Lin Jen-Shin <godfat@godfat.org>
2009-08-21 02:54:37 +0800

    [unit.scala] renamed Endurance to Constitution to avoid initial conflict

--- unit.scala
+++ unit.scala
@@ -1,10 +1,7 @@
 
-import scala.collection.immutable.TreeMap
-import scala.collection.immutable.SortedMap
-
-type ObjectId = Int
-type Env = TreeMap[ObjectId, Unit]
-type Loc = (Int, Int)
+// type ObjectId = Int
+// type Env = TreeMap[ObjectId, Unit]
+// type Loc = (Int, Int)
 
 abstract class Effect
 case class Ampify(val pt: Int) extends Effect
@@ -57,66 +54,58 @@ sealed abstract class Property[T <: Property[T]]{
   def +(p: T): T = create(pt + p.pt)
   def -(p: T): T = create(pt - p.pt)
 }
-case class      Health(val pt: Int) extends Property[Health]      // hp
-{ val create = Health(_) }
-
-case class        Mana(val pt: Int) extends Property[Mana]        // mp
-{ val create = Mana(_) }
-
-case class      Energy(val pt: Int) extends Property[Energy]      // resource
-{ val create = Energy(_) }
-
-case class       Vigor(val pt: Int) extends Property[Vigor]       // action point
-{ val create = Vigor(_) }
-
-case class    Strength(val pt: Int) extends Property[Strength]    // p-atk
-{ val create = Strength(_) }
-
-case class   Endurance(val pt: Int) extends Property[Endurance]   // p-def
-{ val create = Endurance(_) }
-
-case class Imagination(val pt: Int) extends Property[Imagination] // m-atk
-{ val create = Imagination(_) }
-
-case class        Will(val pt: Int) extends Property[Will]        // m-def
-{ val create = Will(_) }
-
-case class     Agility(val pt: Int) extends Property[Agility]     // decides speed in vm
-{ val create = Agility(_) }
+// hp
+case class       Health(val pt: Int) extends Property[      Health]{ val create =       Health(_) }
+// mp
+case class         Mana(val pt: Int) extends Property[        Mana]{ val create =         Mana(_) }
+// resource
+case class       Energy(val pt: Int) extends Property[      Energy]{ val create =       Energy(_) }
+// action point
+case class        Vigor(val pt: Int) extends Property[       Vigor]{ val create =        Vigor(_) }
+// p-atk
+case class     Strength(val pt: Int) extends Property[    Strength]{ val create =     Strength(_) }
+// p-def
+case class Constitution(val pt: Int) extends Property[Constitution]{ val create = Constitution(_) }
+// m-atk
+case class  Imagination(val pt: Int) extends Property[ Imagination]{ val create =  Imagination(_) }
+// m-def
+case class         Will(val pt: Int) extends Property[        Will]{ val create =         Will(_) }
+// decides speed in vm
+case class      Agility(val pt: Int) extends Property[     Agility]{ val create =      Agility(_) }
 
 case class State( val health: Health, val mana: Mana, val energy: Energy, val vigor: Vigor,
-                  val strength: Strength, val endurance: Endurance,
+                  val strength: Strength, val constitution: Constitution,
                   val imagination: Imagination, val will: Will,
                   val agility: Agility ) extends
                  (Health, Mana, Energy, Vigor,
-                  Strength, Endurance, Imagination, Will,
+                  Strength, Constitution, Imagination, Will,
                   Agility)(
                       health, mana, energy, vigor,
-                      strength, endurance, imagination, will,
+                      strength, constitution, imagination, will,
                       agility)
 {
   def +[T <: Property[T]](property: T): State = property match{
-    case h:      Health => State(_1 + h, _2, _3, _4, _5, _6, _7, _8, _9)
-    case m:        Mana => State(_1, _2 + m, _3, _4, _5, _6, _7, _8, _9)
-    case e:      Energy => State(_1, _2, _3 + e, _4, _5, _6, _7, _8, _9)
-    case v:       Vigor => State(_1, _2, _3, _4 + v, _5, _6, _7, _8, _9)
-    case s:    Strength => State(_1, _2, _3, _4, _5 + s, _6, _7, _8, _9)
-    case e:   Endurance => State(_1, _2, _3, _4, _5, _6 + e, _7, _8, _9)
-    case i: Imagination => State(_1, _2, _3, _4, _5, _6, _7 + i, _8, _9)
-    case w:        Will => State(_1, _2, _3, _4, _5, _6, _7, _8 + w, _9)
-    case a:     Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 + a)
+    case h:       Health => State(_1 + h, _2, _3, _4, _5, _6, _7, _8, _9)
+    case m:         Mana => State(_1, _2 + m, _3, _4, _5, _6, _7, _8, _9)
+    case e:       Energy => State(_1, _2, _3 + e, _4, _5, _6, _7, _8, _9)
+    case v:        Vigor => State(_1, _2, _3, _4 + v, _5, _6, _7, _8, _9)
+    case s:     Strength => State(_1, _2, _3, _4, _5 + s, _6, _7, _8, _9)
+    case c: Constitution => State(_1, _2, _3, _4, _5, _6 + c, _7, _8, _9)
+    case i:  Imagination => State(_1, _2, _3, _4, _5, _6, _7 + i, _8, _9)
+    case w:         Will => State(_1, _2, _3, _4, _5, _6, _7, _8 + w, _9)
+    case a:      Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 + a)
   }
 
   def -[T <: Property[T]](property: T): State = property match{
-    case h:      Health => State(_1 - h, _2, _3, _4, _5, _6, _7, _8, _9)
-    case m:        Mana => State(_1, _2 - m, _3, _4, _5, _6, _7, _8, _9)
-    case e:      Energy => State(_1, _2, _3 - e, _4, _5, _6, _7, _8, _9)
-    case v:       Vigor => State(_1, _2, _3, _4 - v, _5, _6, _7, _8, _9)
-    case s:    Strength => State(_1, _2, _3, _4, _5 - s, _6, _7, _8, _9)
-    case e:   Endurance => State(_1, _2, _3, _4, _5, _6 - e, _7, _8, _9)
-    case i: Imagination => State(_1, _2, _3, _4, _5, _6, _7 - i, _8, _9)
-    case w:        Will => State(_1, _2, _3, _4, _5, _6, _7, _8 - w, _9)
-    case a:     Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 - a)
+    case h:       Health => State(_1 - h, _2, _3, _4, _5, _6, _7, _8, _9)
+    case m:         Mana => State(_1, _2 - m, _3, _4, _5, _6, _7, _8, _9)
+    case e:       Energy => State(_1, _2, _3 - e, _4, _5, _6, _7, _8, _9)
+    case v:        Vigor => State(_1, _2, _3, _4 - v, _5, _6, _7, _8, _9)
+    case s:     Strength => State(_1, _2, _3, _4, _5 - s, _6, _7, _8, _9)
+    case c: Constitution => State(_1, _2, _3, _4, _5, _6 - c, _7, _8, _9)
+    case i:  Imagination => State(_1, _2, _3, _4, _5, _6, _7 - i, _8, _9)
+    case w:         Will => State(_1, _2, _3, _4, _5, _6, _7, _8 - w, _9)
+    case a:      Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 - a)
   }
 }
 
@@ -136,7 +125,7 @@ abstract class Terrain(     name: String,
   def this(name: String, elements: List[Element]) =
       this(name, elements,
              State(Health(999), Mana(0), Energy(0), Vigor(0),
-                   Strength(0), Endurance(99), Imagination(0), Will(99),
+                   Strength(0), Constitution(99), Imagination(0), Will(99),
                    Agility(0)))
 }
 
@@ -149,7 +138,7 @@ case class Plains() extends Terrain("Plains", List(  Air.Small,   Air.Small))
 val Footman = new Unit( "Footman",
                         List(Fire.Large),
                         State(Health(100), Mana(10), Energy(50), Vigor(50),
-                              Strength(40), Endurance(20), Imagination(10), Will(10),
+                              Strength(40), Constitution(20), Imagination(10), Will(10),
                               Agility(15)))
 
 println(Fire.Large(Water.Small))
