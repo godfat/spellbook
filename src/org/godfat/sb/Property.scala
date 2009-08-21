a67c1d84ff174b5e1fc77f2c963d06a04e768de0
Lin Jen-Shin <godfat@godfat.org>
2009-08-22 00:06:30 +0800

    [Property.scala] renamed type T to This

--- src/org/godfat/sb/Property.scala
+++ src/org/godfat/sb/Property.scala
@@ -2,55 +2,55 @@
 package org.godfat.sb
 
 sealed abstract class Property{
-  type T <: Property
+  type This <: Property
   val pt: Int
-  val create: Int => T
+  val create: Int => This
 
-  def +(p: T): T = create(pt + p.pt)
-  def -(p: T): T = create(pt - p.pt)
+  def +(p: This): This = create(pt + p.pt)
+  def -(p: This): This = create(pt - p.pt)
 }
 // hp
 case class     Health      (val pt: Int) extends Property{
-  type     T = Health
+  type  This = Health
   val create = Health(_)
 }
 // mp
 case class     Mana        (val pt: Int) extends Property{
-  type     T = Mana
+  type  This = Mana
   val create = Mana(_)
 }
 // resource
 case class     Energy      (val pt: Int) extends Property{
-  type     T = Energy
+  type  This = Energy
   val create = Energy(_)
 }
 // action point
 case class     Vigor       (val pt: Int) extends Property{
-  type     T = Vigor
+  type  This = Vigor
   val create = Vigor(_)
 }
 // p-atk
 case class     Strength    (val pt: Int) extends Property{
-  type     T = Strength
+  type  This = Strength
   val create = Strength(_)
 }
 // p-def
 case class     Constitution(val pt: Int) extends Property{
-  type     T = Constitution
+  type  This = Constitution
   val create = Constitution(_)
 }
 // m-atk
 case class     Imagination (val pt: Int) extends Property{
-  type     T = Imagination
+  type  This = Imagination
   val create = Imagination(_)
 }
 // m-def
 case class     Will        (val pt: Int) extends Property{
-  type     T = Will
+  type  This = Will
   val create = Will(_)
 }
 // decides speed in vm
 case class     Agility     (val pt: Int) extends Property{
-  type     T = Agility
+  type  This = Agility
   val create = Agility(_)
 }
