a981a90fc3b85f0ec56f71ff8ab2006d7ce54772
Lin Jen-Shin <godfat@godfat.org>
2009-08-22 01:30:21 +0800

    [test.scala] test about Terrain(River)

--- bin/test.scala
+++ bin/test.scala
@@ -7,14 +7,17 @@ import org.godfat.sb.Fire
 import org.godfat.sb.Water
 import org.godfat.sb.Footman
 import org.godfat.sb.Health
+import org.godfat.sb.River
+import org.godfat.sb.TerrainState
 
 abstract class Buff
 case class Enrage() extends Buff
 
 println(Fire.Large(Water.Small))
-println(Footman)
+println(Footman.name)
 println(Footman - Health(10))
 println(Footman.state.health - Health(5))
+println(River(TerrainState) - Health(50))
 
 // abstract class Action
 // case class   Move extends Action
