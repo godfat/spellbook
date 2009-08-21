41986d6ee0a47538c928d72e7d08132568076a48
Lin Jen-Shin <godfat@godfat.org>
2009-08-21 18:38:54 +0800

    [Property.scala, State.scala] extracted State.scala from Property.scala

---
+++ src/org/godfat/sb/State.scala
@@ -0,0 +1,38 @@
+
+package org.godfat.sb
+
+case class State( val health: Health, val mana: Mana, val energy: Energy, val vigor: Vigor,
+                  val strength: Strength, val constitution: Constitution,
+                  val imagination: Imagination, val will: Will,
+                  val agility: Agility ) extends
+                 (Health, Mana, Energy, Vigor,
+                  Strength, Constitution, Imagination, Will,
+                  Agility)(
+                      health, mana, energy, vigor,
+                      strength, constitution, imagination, will,
+                      agility)
+{
+  def +[T <: Property[T]](property: T): State = property match{
+    case h:       Health => State(_1 + h, _2, _3, _4, _5, _6, _7, _8, _9)
+    case m:         Mana => State(_1, _2 + m, _3, _4, _5, _6, _7, _8, _9)
+    case e:       Energy => State(_1, _2, _3 + e, _4, _5, _6, _7, _8, _9)
+    case v:        Vigor => State(_1, _2, _3, _4 + v, _5, _6, _7, _8, _9)
+    case s:     Strength => State(_1, _2, _3, _4, _5 + s, _6, _7, _8, _9)
+    case c: Constitution => State(_1, _2, _3, _4, _5, _6 + c, _7, _8, _9)
+    case i:  Imagination => State(_1, _2, _3, _4, _5, _6, _7 + i, _8, _9)
+    case w:         Will => State(_1, _2, _3, _4, _5, _6, _7, _8 + w, _9)
+    case a:      Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 + a)
+  }
+
+  def -[T <: Property[T]](property: T): State = property match{
+    case h:       Health => State(_1 - h, _2, _3, _4, _5, _6, _7, _8, _9)
+    case m:         Mana => State(_1, _2 - m, _3, _4, _5, _6, _7, _8, _9)
+    case e:       Energy => State(_1, _2, _3 - e, _4, _5, _6, _7, _8, _9)
+    case v:        Vigor => State(_1, _2, _3, _4 - v, _5, _6, _7, _8, _9)
+    case s:     Strength => State(_1, _2, _3, _4, _5 - s, _6, _7, _8, _9)
+    case c: Constitution => State(_1, _2, _3, _4, _5, _6 - c, _7, _8, _9)
+    case i:  Imagination => State(_1, _2, _3, _4, _5, _6, _7 - i, _8, _9)
+    case w:         Will => State(_1, _2, _3, _4, _5, _6, _7, _8 - w, _9)
+    case a:      Agility => State(_1, _2, _3, _4, _5, _6, _7, _8, _9 - a)
+  }
+}
