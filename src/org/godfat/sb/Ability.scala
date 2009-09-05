f4e51be50adca76cf655b8524745dc4bf8748bba
Lin Jen-Shin <godfat@godfat.org>
2009-09-06 01:14:44 +0800

    [Ability.scala] introduce AbilitySimple, extracted from MeleeAttack:
      and now AbilityActive and AbilityPassive are traits,
      since they are not overriding anything, they don't have to be
      put into the family.

--- src/org/godfat/sb/Ability.scala
+++ src/org/godfat/sb/Ability.scala
@@ -12,25 +12,34 @@ abstract class Ability{
   def  select(map: Map, to: Block): List[Block] = List(to)
   def   apply(from: Block, to: Block): Block = to
 }
-abstract class AbilityActive  extends Ability
-abstract class AbilityPassive extends Ability
 
-case class Move()         extends AbilityActive
-case class Wait()         extends AbilityActive
+abstract class AbilitySimple[Cost <: Property, Damage <: Property] extends Ability{
+  def   cost(fc: Creature): Cost
+  def damage(fc: Creature, tc: Creature): Damage
 
-case class MeleeAttack()  extends AbilityActive{
   override def consume(from: Block): Block = from.creature match{
-    case Just(c: Creature) => Block( from.index, from.terrain, Just(c - Vigor(10)) )
+    case Just(c: Creature) => Block( from.index, from.terrain, Just(c - cost(c)) )
     case _                 => error("no creature for consume")
   }
 
   override def apply(from: Block, to: Block): Block = (from.creature, to.creature) match{
-    case (Just(fc: Creature), Just(tc: Creature)) => {
-      val damage: Int = fc.state.strength.pt - tc.state.constitution.pt
-      Block( to.index, to.terrain, Just(tc - Health(damage)) )
-    }
-    case _                 => error("missing creature(s) for melee attack")
+    case (Just(fc: Creature), Just(tc: Creature)) =>
+      Block( to.index, to.terrain, Just(tc - damage(fc, tc)) )
+
+    case _                 => error("missing creature(s)")
   }
+}
+
+trait AbilityActive  extends Ability
+trait AbilityPassive extends Ability
+
+case class Move()    extends AbilityActive
+case class Wait()    extends AbilityActive
+
+case class MeleeAttack() extends AbilitySimple[Vigor, Health] with AbilityActive{
+  override def   cost(fc: Creature) = Vigor(10)
+  override def damage(fc: Creature, tc: Creature) =
+    Health(fc.state.strength.pt - tc.state.constitution.pt)
 }
 
 case class RangedAttack() extends AbilityActive
