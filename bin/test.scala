49108347167c57ed1be29d2071a7f18186e4799a
Lin Jen-Shin <godfat@godfat.org>
2010-01-12 02:37:58 +0800

    [test.scala] println me all true

--- bin/test.scala
+++ bin/test.scala
@@ -10,25 +10,25 @@ import org.godfat.sb.prelude._
 
 import scala.collection.immutable.TreeMap
 
-println(Fire.Innate(Water.Innate)) // Ampify(-200)
-println(Footman.name) // Footman
-println((Footman - Health(10)).state.health) // Health(90)
-println(Footman.state.health - Health(5)) // Health(95)
-println((River() - Health(50)).state.health) // Health(949)
+println(Fire.Innate(Water.Innate) == Ampify(-200)) // Ampify(-200)
+println(Footman.name == "Footman") // Footman
+println((Footman - Health(10)).state.health == Health(90)) // Health(90)
+println(Footman.state.health - Health(5) == Health(95)) // Health(95)
+println((River() - Health(50)).state.health == Health(949)) // Health(949)
 
 val b = Block(0, River() + Energy(10), Nothing).put_on(Footman)
-println(b.terrain.state.energy, b.creature.get.state.energy) // (Energy(0),Energy(60))
+println((b.terrain.state.energy, b.creature.get.state.energy) == (Energy(0),Energy(60))) // (Energy(0),Energy(60))
 
 val from = Block(0, River(), Just(Footman))
 val to   = Block(1, River(), Just(Footman))
 val map  = Map(2, 1, TreeMap(0 -> from, 1 -> to))
 
 val (bb, lb) = AttackMelee().activate(map, from, to)
-println((bb.creature.get.state.vigor, lb.head.creature.get.state.health)) // (Vigor(40),Health(80))
+println((bb.creature.get.state.vigor, lb.head.creature.get.state.health) == (Vigor(40),Health(80))) // (Vigor(40),Health(80))
 
 val map2 = Map.create(5, 5, Block(_, River(), Nothing))
 
-println(map2.nearby(12, 2).map(_.index))
+println(map2.nearby(12, 2).map(_.index) == List(1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23))
 // List(1, 2, 3, 5, 6, 7, 8, 10, 11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23)
 //  00     (01)    (02)    (03)     04
 //     (05)    (06)    (07)    (08)     09
@@ -36,7 +36,7 @@ println(map2.nearby(12, 2).map(_.index))
 //     (15)    (16)    (17)    (18)     19
 //  20     (21)    (22)    (23)     24
 
-println(map2.nearby(13, 2).map(_.index))
+println(map2.nearby(13, 2).map(_.index) == List(2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19, 22, 23, 24))
 // List(2, 3, 4, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 19, 22, 23, 24)
 //  00      01     (02)    (03)    (04)
 //      05     (06)    (07)    (08)    (09)
@@ -44,7 +44,7 @@ println(map2.nearby(13, 2).map(_.index))
 //      15     (16)    (17)    (18)    (19)
 //  20      21     (22)    (23)    (24)
 
-println(map2.nearby(1, 2).map(_.index))
+println(map2.nearby(1, 2).map(_.index) == List(0, 1, 2, 3, 5, 6, 7, 10, 11, 12))
 // List(0, 1, 2, 3, 5, 6, 7, 10, 11, 12)
 // (00)    (01)    (02)    (03)     04
 //     (05)    (06)    (07)     08      09
@@ -54,9 +54,9 @@ println(map2.nearby(1, 2).map(_.index))
 
 println(CreatureMap("Footman") == Footman) // true
 
-println(Absorb(10) + Absorb(-10)) // Absorb(0)
-println(Footman.affect(Footman)) // (Ampify(0),Absorb(200))
-println(Footman.affect(River())) // (Ampify(-200),Absorb(0))
+println(Absorb(10) + Absorb(-10) == Absorb(0)) // Absorb(0)
+println(Footman.affect(Footman) == (Ampify(0),Absorb(200))) // (Ampify(0),Absorb(200))
+println(Footman.affect(River()) == (Ampify(-200),Absorb(0))) // (Ampify(-200),Absorb(0))
 
 // walk_to :: Path -> Creature -> Path
 // walk_to path creature =
