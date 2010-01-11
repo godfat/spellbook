c5d99ca54f10ee40426c01babbd69a4c26066c77
Lin Jen-Shin <godfat@godfat.org>
2010-01-12 01:13:40 +0800

    update scala

--- bin/check.sh
+++ bin/check.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-scala-2.8.0.r18549-b20090823020847/bin/scala -cp \
+scala-2.8.0.r20436-b20100111020117/bin/scala -cp \
 build/lib/spellbook.jar:ScalaCheck-1.5-Scala2.8.0.r17999-b20090605202829.jar \
 bin/check.scala
