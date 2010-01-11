82fddc20810995ef97866fc57f338f2786705c33
Lin Jen-Shin <godfat@godfat.org>
2010-01-12 02:54:01 +0800

    [Block.scala, State.scala] warning elimination

--- src/org/godfat/sb/Block.scala
+++ src/org/godfat/sb/Block.scala
@@ -3,8 +3,7 @@ package org.godfat.sb
 
 import prelude._
 
-case class Block(val index: Int, val terrain: Terrain, val creature: Maybe[Creature]) extends
-  (Terrain, Maybe[Creature])(terrain, creature) with Ordered[Block]
+case class Block(val index: Int, val terrain: Terrain, val creature: Maybe[Creature]) extends Ordered[Block]
 {
   def compare(that: Block) = this.index.compare(that.index)
 
--- src/org/godfat/sb/State.scala
+++ src/org/godfat/sb/State.scala
@@ -4,41 +4,36 @@ package org.godfat.sb
 case class State( health: Health, mana: Mana, energy: Energy, vigor: Vigor,
                   strength: Strength, constitution: Constitution,
                   imagination: Imagination, will: Will,
-                  agility: Agility ) extends
-  (Health, Mana, Energy, Vigor,
-   Strength, Constitution, Imagination, Will,
-   Agility)(      health, mana, energy, vigor,
-                  strength, constitution, imagination, will,
-                  agility )
+                  agility: Agility )
 {
   def +(property: Property): State = property match{
-    case h:       Health => create(      health = _1 + h)
-    case m:         Mana => create(        mana = _2 + m)
-    case e:       Energy => create(      energy = _3 + e)
-    case v:        Vigor => create(       vigor = _4 + v)
-    case s:     Strength => create(    strength = _5 + s)
-    case c: Constitution => create(constitution = _6 + c)
-    case i:  Imagination => create( imagination = _7 + i)
-    case w:         Will => create(        will = _8 + w)
-    case a:      Agility => create(     agility = _9 + a)
+    case h:       Health => create(      health =       health + h)
+    case m:         Mana => create(        mana =         mana + m)
+    case e:       Energy => create(      energy =       energy + e)
+    case v:        Vigor => create(       vigor =        vigor + v)
+    case s:     Strength => create(    strength =     strength + s)
+    case c: Constitution => create(constitution = constitution + c)
+    case i:  Imagination => create( imagination =  imagination + i)
+    case w:         Will => create(        will =         will + w)
+    case a:      Agility => create(     agility =      agility + a)
   }
 
   def -(property: Property): State = property match{
-    case h:       Health => create(      health = _1 - h)
-    case m:         Mana => create(        mana = _2 - m)
-    case e:       Energy => create(      energy = _3 - e)
-    case v:        Vigor => create(       vigor = _4 - v)
-    case s:     Strength => create(    strength = _5 - s)
-    case c: Constitution => create(constitution = _6 - c)
-    case i:  Imagination => create( imagination = _7 - i)
-    case w:         Will => create(        will = _8 - w)
-    case a:      Agility => create(     agility = _9 - a)
+    case h:       Health => create(      health =       health - h)
+    case m:         Mana => create(        mana =         mana - m)
+    case e:       Energy => create(      energy =       energy - e)
+    case v:        Vigor => create(       vigor =        vigor - v)
+    case s:     Strength => create(    strength =     strength - s)
+    case c: Constitution => create(constitution = constitution - c)
+    case i:  Imagination => create( imagination =  imagination - i)
+    case w:         Will => create(        will =         will - w)
+    case a:      Agility => create(     agility =      agility - a)
   }
 
-  def create(health: Health = _1, mana: Mana = _2, energy: Energy = _3, vigor: Vigor = _4,
-             strength: Strength = _5, constitution: Constitution = _6,
-             imagination: Imagination = _7, will: Will = _8,
-             agility: Agility = _9) =
+  def create(health: Health = health, mana: Mana = mana, energy: Energy = energy, vigor: Vigor = vigor,
+             strength: Strength = strength, constitution: Constitution = constitution,
+             imagination: Imagination = imagination, will: Will = will,
+             agility: Agility = agility) =
       State(health, mana, energy, vigor,
             strength, constitution, imagination, will,
             agility)
