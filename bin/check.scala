190ae9fabdcd2df89def738423bc1280f81e0226
Lin Jen-Shin <godfat@godfat.org>
2010-01-20 23:00:09 +0800

    [bin/check.scala] more sophisticated check. TODO: add to ant

--- bin/check.scala
+++ bin/check.scala
@@ -4,7 +4,7 @@ import org.scalacheck._
 
 import org.spbk.pure._
 
-object CheckElement{
+object CheckElement extends Properties("Element"){
   val gen = Gen.oneOf(Earth.Innate, Earth.Acquired,
                       Water.Innate, Water.Acquired,
                        Fire.Innate,  Fire.Acquired,
@@ -13,32 +13,31 @@ object CheckElement{
 
   implicit def arbEle: Arbitrary[Element] = Arbitrary(gen)
 
-  def check =
-    forAll{ (a: Element, b: Element) =>
-      a(b) == (
-        if(a.name == b.name){
-          Absorb(a.pt + b.pt)
+  property("apply") = Prop.forAll( (a: Element, b: Element) =>
+    a(b) == (
+      if(a.name == b.name){
+        Absorb(a.pt + b.pt)
+      }
+      else{
+        (a, b) match{
+          case (e0: Earth, e1: Water) => Ampify(a.pt + b.pt)
+          case (e0: Water, e1:  Fire) => Ampify(a.pt + b.pt)
+          case (e0:  Fire, e1:   Air) => Ampify(a.pt + b.pt)
+          case (e0:   Air, e1: Earth) => Ampify(a.pt + b.pt)
+
+          case (e0: Earth, e1:   Air) => Ampify(- a.pt - b.pt)
+          case (e0: Water, e1: Earth) => Ampify(- a.pt - b.pt)
+          case (e0:  Fire, e1: Water) => Ampify(- a.pt - b.pt)
+          case (e0:   Air, e1:  Fire) => Ampify(- a.pt - b.pt)
+
+          case (e0: Light,         _) => Ampify(a.pt + b.pt)
+          case (        _, e1: Light) => Ampify(a.pt + b.pt)
+
+          case                      _ => Ampify(0)
         }
-        else{
-          (a, b) match{
-            case (e0: Earth, e1: Water) => Ampify(a.pt + b.pt)
-            case (e0: Water, e1:  Fire) => Ampify(a.pt + b.pt)
-            case (e0:  Fire, e1:   Air) => Ampify(a.pt + b.pt)
-            case (e0:   Air, e1: Earth) => Ampify(a.pt + b.pt)
-
-            case (e0: Earth, e1:   Air) => Ampify(- a.pt - b.pt)
-            case (e0: Water, e1: Earth) => Ampify(- a.pt - b.pt)
-            case (e0:  Fire, e1: Water) => Ampify(- a.pt - b.pt)
-            case (e0:   Air, e1:  Fire) => Ampify(- a.pt - b.pt)
-
-            case (e0: Light,         _) => Ampify(a.pt + b.pt)
-            case (        _, e1: Light) => Ampify(a.pt + b.pt)
-
-            case                      _ => Ampify(0)
-          }
-        }
-      )
-    }.check
+      }
+    )
+  )
 }
 
-CheckElement.check
+CheckElement.main(new Array(0))
