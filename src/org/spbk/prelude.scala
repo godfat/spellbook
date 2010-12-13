c4712879ffc8f1dbad9e956443b501585472ef79
Lin Jen-Shin <godfat@godfat.org>
2010-12-14 01:27:58 +0800

    move CreatureMap into prelude

--- src/org/spbk/prelude.scala
+++ src/org/spbk/prelude.scala
@@ -6,4 +6,18 @@ package object prelude{
   type    Just[+A] =   Some[A]
    val    Just     =   Some
    val Nothing     =   None
+
+  import org.spbk.pure._
+  import scala.collection.immutable.TreeMap
+
+  object Footman extends Creature( "Footman",
+                                   List(Fire.innate),
+                                   State(Health(100), Mana(10), Energy(50), Vigor(50),
+                                   Strength(40), Constitution(20), Imagination(10), Will(10),
+                                   Agility(15)),
+                                   List(AttackMelee()) )
+
+  val CreatureMap: TreeMap[String, Creature] = TreeMap[String, Creature]() ++ List(
+      Footman.name -> Footman
+    )
 }
